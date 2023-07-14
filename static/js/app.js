89af8152d5c1d0a2fb487501f2abc84172f3b595
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-14 17:13:55 +0300

    loop over animal data

--- static/js/app.js
+++ static/js/app.js
@@ -53,7 +53,23 @@ class AnimalApp {
     }
   
     handleDisplay(data) {
- 
+      this.animalList.innerHTML = '';
+  
+      data.forEach(animal => {
+        const animalItem = document.createElement('li');
+        animalItem.classList.add('card');
+        animalItem.innerHTML = `
+          <h3>${animal.nameInput}</h3>
+          <img src="${animal.imageInput}" alt="${animal.name}">
+          <p>${animal.descriptionInput}</p>
+          <p>Donations: ${animal.donations}</p>
+          <button class="editBtn" data-id="${animal.id}">Edit</button>
+          <button class="donateBtn" data-id="${animal.id}">Donation</button>
+          <button class="deleteBtn" data-id="${animal.id}">Delete</button>
+        `;
+  
+        this.animalList.appendChild(animalItem);
+      });
     }
   
   }
