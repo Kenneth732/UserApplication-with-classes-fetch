b9b386705489426212c6fbe2928e5a1c14993955
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-14 17:07:32 +0300

    animal-constructor pro..

---
+++ static/js/app.js
@@ -0,0 +1,23 @@
+class AnimalApp {
+    constructor() {
+      this.animalList = document.querySelector('#animal-list');
+      this._nameInput = document.querySelector('#nameInput');
+      this._imageInput = document.querySelector('#imageInput');
+      this._descriptionInput = document.querySelector('#descriptionInput');
+      this.animals = [];
+  
+      this.handleFormSubmit = this.handleFormSubmit.bind(this);
+      this.handleDeleteRequest = this.handleDeleteRequest.bind(this);
+      this.handleEditRequest = this.handleEditRequest.bind(this);
+      this.handleDonateRequest = this.handleDonateRequest.bind(this);
+  
+      document.querySelector('#animal-form').addEventListener('submit', this.handleFormSubmit);
+      this.handleRenderFetch();
+    }
+  
+
+  }
+  
+  // Create an instance of the AnimalApp class
+  const animalApp = new AnimalApp();
+  
