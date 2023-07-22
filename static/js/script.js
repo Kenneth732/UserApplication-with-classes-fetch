61fe22a92ebedafc0a5cf3865f5ae931341929cc
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-22 14:10:11 +0300

    selectors

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,20 @@
 class AnimalApp {
-    constructor() 
+    constructor() {
+      this.animalList = document.querySelector('#animal-list');
+      this.nameInput = document.querySelector('#nameInput');
+      this.imageInput = document.querySelector('#imageInput');
+      this.descriptionInput = document.querySelector('#descriptionInput');
 
+  
+      this.handleFormSubmit = this.handleFormSubmit.bind(this);
+      this.handleDeleteRequest = this.handleDeleteRequest.bind(this);
+      this.handleEditRequest = this.handleEditRequest.bind(this);
+      this.handleDonateRequest = this.handleDonateRequest.bind(this);
+  
+      document.querySelector('#animal-form').addEventListener('submit', this.handleFormSubmit);
+      this.handleRenderFetch();
+    }
+  
   }
   
   // Create an instance of the AnimalApp class
